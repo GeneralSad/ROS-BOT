badac835727e95f884b0869cb9bff9b23dabda52
TomKaas <tomkaas74@gmail.com>
2023-03-27 02:39:51 -0700

    test publisher for sensor

--- src/publisher.cpp
+++ src/publisher.cpp
@@ -1,21 +1,42 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include <iostream>
+#include "std_msgs/Float32MultiArray.h"
 
-int main(int argc, char **argv){
+#define GND_PIN 0
+#define DATA_PIN 1
+#define I2C_ADDRESS 0x5A
+#define BAUD_RATE 9600
 
-	ros::init(argc,argv,"mypublishernode2");
-	ros::NodeHandle n;
-	ros::Publisher publisher = n.advertise<std_msgs::String>("/mytopic2", 1000);
-	ros::Rate loop_rate(3); //frequentie Hz
-	
-	while (ros::ok()){
-		std_msgs::String msg;
-		msg.data = "mymessage2!";
-		publisher.publish(msg);
-		ros::spinOnce();
-		loop_rate.sleep();
-	}
-	
-	return 0;
-	
+float CO2Level = 0.0f;
+float TVOCLevel = 0.0f;
+
+void getAirQuality() {
+    //get sensor data, first value CO2, second TVOC, third temperature, Temp is not Supported with current sensor version
+    //CO2Level = data[0] uitegelezen van pin
+    //TVOCLevel = data[1]
+
+    CO2Level = CO2Level + 2.5f;
+    TVOCLevel = TVOCLevel + 0.5f;
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "mypublishernode2");
+    ros::NodeHandle n;
+    ros::Publisher publisher = n.advertise<std_msgs::Float32MultiArray>("/AirQuality", 1000);
+    ros::Rate loop_rate(1); // frequentie Hz
+    // std::cout << "Publisher running" << std::endl;
+    while (ros::ok())
+    {
+        std_msgs::Float32MultiArray data;
+        // get data from sensor, for now just raise
+        getAirQuality();
+        data.data.push_back(CO2Level);
+        data.data.push_back(TVOCLevel);
+        publisher.publish(data);
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+    return 0;
 }
